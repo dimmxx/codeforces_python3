f7ae6f376ff89bbe43da009d49dcff910e4df6e1
dimmxx <dimmxx@gmail.com>
2018-09-28 18:24:38 +0300

    29-09-18

--- 266B_Queue AtTheSchool.py
+++ 266B_Queue AtTheSchool.py
@@ -1,9 +1,20 @@
-ql = int(input())
-sec = int(input())
+ql, sec = map(int, input().split())
 que = list(input())
 
+db = []
 
-change = 0
-for i in range (len(que) - 1):
-    if que[i] == 'B' and que[i + 1] == 'G':
-        i += 2
+while sec > 0:
+    for i in range(len(que) - 1):
+         if que[i] == 'B' and que[i + 1] == 'G':
+             db.append(i)
+
+    for i in range(len(db)):
+         temp = que[db[i]]
+         que[db[i]] = que[db[i] + 1]
+         que[db[i] + 1] = temp
+    db =[]
+    sec -= 1
+
+print(''.join(que))
+
+

919324dec920c668a7e6b4ec643cff023ff8157d
dimmxx <dimmxx@gmail.com>
2018-10-01 17:20:31 +0300

    01.10.18

---
+++ 1047A_LittleCLoves3.py
@@ -0,0 +1,24 @@
+import random
+
+number = int(input())
+
+number1 = random.randint(0, number)
+if number1 % 3 == 0:
+    number1 -= 1
+
+number2 = random.randint(0, number - number1)
+if number2 % 3 == 0:
+    number2 -= 1
+
+number3 = number - number1 - number2
+if number3 % 3 == 0:
+    number1 = 1
+    number2 = 1
+    number3 = number - 2
+    if number3 % 3 == 0:
+       number3 -= 1
+       number1 += 1
+
+
+print(number1, number2, number3)
+
---
+++ temp.py
@@ -0,0 +1,11 @@
+import random
+
+
+number = int(input())
+
+number1 = random.randint(0, number)
+
+
+
+
+print(number1, number2, number3)

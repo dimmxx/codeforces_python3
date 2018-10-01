f415607f6988ac1b3d84cc90172ad84e3a08fb3a
dimmxx <dimmxx@gmail.com>
2018-10-01 07:20:20 +0300

    01.10.18

--- 41A_Translation.py
+++ 41A_Translation.py
@@ -1,12 +1,11 @@
-
 word = input()
 code = input()
 code_m = []
 
-
 for i in range(len(word) - 1, -1, -1):
     code_m.append(word[i])
 
+code_m = ''.join(code_m)
 
 if code_m == code:
     print('YES')

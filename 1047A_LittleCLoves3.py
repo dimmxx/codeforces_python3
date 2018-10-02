46e1de589fee8b87aa37e84c50a570f9882ee77f
dimmxx <dimmxx@gmail.com>
2018-10-02 06:46:29 +0300

    02.10.18

--- 1047A_LittleCLoves3.py
+++ 1047A_LittleCLoves3.py
@@ -4,14 +4,14 @@ number = int(input())
 
 number1 = random.randint(0, number)
 if number1 % 3 == 0:
-    number1 -= 1
+    number1 += 1
 
 number2 = random.randint(0, number - number1)
 if number2 % 3 == 0:
-    number2 -= 1
+    number2 += 1
 
 number3 = number - number1 - number2
-if number3 % 3 == 0:
+if number3 % 3 == 0 or number3 < 0:
     number1 = 1
     number2 = 1
     number3 = number - 2
